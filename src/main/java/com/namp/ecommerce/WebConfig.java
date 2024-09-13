430e7405e5c4c2b3c8261bcd357698bd923a8f37
gonzabaldassi <gonzalobaldassi02@gmail.com>
2024-09-13 13:38:59 -0300

    WebConfig

--- src/main/java/com/namp/ecommerce/WebConfig.java
+++ src/main/java/com/namp/ecommerce/WebConfig.java
@@ -1,9 +1,10 @@
 package com.namp.ecommerce;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -11,10 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
         // Configuracion del acceso a la carpeta de imagenes
         registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
     }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**") // Permite todas las rutas de la API
                 .allowedOrigins("http://localhost:3000") // Dirección del frontend
-                .allowedMethods("");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permite el envío de cookies y credenciales
     }
 }
