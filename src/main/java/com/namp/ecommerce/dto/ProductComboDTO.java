bec2b50b4f1f2cff0a83c0bd44254ca579aa06d5
agustin06 <anil.agustin06@gmail.com>
2024-09-24 00:10:10 -0300

    Test Category (incomplete)

--- src/main/java/com/namp/ecommerce/dto/ProductComboDTO.java
+++ src/main/java/com/namp/ecommerce/dto/ProductComboDTO.java
@@ -1,8 +1,12 @@
 package com.namp.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductComboDTO {
     private long idProductCombo;
     private ProductDTO idProduct;
--- src/main/java/com/namp/ecommerce/dto/ProductWithItDTO.java
+++ src/main/java/com/namp/ecommerce/dto/ProductWithItDTO.java
@@ -1,10 +1,14 @@
 package com.namp.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductWithItDTO {
     private long idProduct;
     private String name;
